457c2dd067b7ed24e24af86c7b1c12f314b9d6cd
Lakshmi Narayana Gupta Dogiparthi <lakshmi.dogiparthi@citiustech.com>
2023-04-19 14:34:07 +0530

    gfdg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,48 +18,48 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { NavMobComponent } from './nav-bar/nav-mob/nav-mob.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NotifierModule, NotifierOptions } from 'angular-notifier'
+// import { NotifierModule, NotifierOptions } from 'angular-notifier'
 
-const customNotifierOptions: NotifierOptions = {
-  position: {
-		horizontal: {
-			position: 'right',
-			distance: 12
-		},
-		vertical: {
-			position: 'top',
-			distance: 12,
-			gap: 10
-		}
-	},
-  theme: 'material',
-  behaviour: {
-    autoHide: 5000,
-    onClick: 'hide',
-    onMouseover: 'pauseAutoHide',
-    showDismissButton: true,
-    stacking: 4
-  },
-  animations: {
-    enabled: true,
-    show: {
-      preset: 'slide',
-      speed: 300,
-      easing: 'ease'
-    },
-    hide: {
-      preset: 'fade',
-      speed: 300,
-      easing: 'ease',
-      offset: 50
-    },
-    shift: {
-      speed: 300,
-      easing: 'ease'
-    },
-    overlap: 150
-  }
-};
+// const customNotifierOptions: NotifierOptions = {
+//   position: {
+// 		horizontal: {
+// 			position: 'right',
+// 			distance: 12
+// 		},
+// 		vertical: {
+// 			position: 'top',
+// 			distance: 12,
+// 			gap: 10
+// 		}
+// 	},
+//   theme: 'material',
+//   behaviour: {
+//     autoHide: 5000,
+//     onClick: 'hide',
+//     onMouseover: 'pauseAutoHide',
+//     showDismissButton: true,
+//     stacking: 4
+//   },
+//   animations: {
+//     enabled: true,
+//     show: {
+//       preset: 'slide',
+//       speed: 300,
+//       easing: 'ease'
+//     },
+//     hide: {
+//       preset: 'fade',
+//       speed: 300,
+//       easing: 'ease',
+//       offset: 50
+//     },
+//     shift: {
+//       speed: 300,
+//       easing: 'ease'
+//     },
+//     overlap: 150
+//   }
+// };
 
 @NgModule({
   declarations: [
@@ -83,7 +83,7 @@ const customNotifierOptions: NotifierOptions = {
     MatCardModule,
     MatTabsModule,
     HttpClientModule,
-    NotifierModule.withConfig(customNotifierOptions)
+   // NotifierModule.withConfig(customNotifierOptions)
   ],
   providers: [],
   bootstrap: [AppComponent]
